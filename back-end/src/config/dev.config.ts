f99851900a4aba089b4a91d7943d5e63c11f616c
ibjelic <bjelic.ilija@yahoo.com>
2022-05-18 20:43:49 +0200

    Config files ts

--- back-end/src/config/dev.config.ts
+++ back-end/src/config/dev.config.ts
@@ -3,7 +3,16 @@ import IConfig from "./IConfig.interface"
 const DevConfig: IConfig = {
     server : {
         port: 10000
-    }
+    }, 
+    static : {
+        index: false,
+        dotfiles: "deny",
+        cacheControl: true,
+        etag: true,
+        maxAge: 1000* 60* 60* 24,
+        path: "./static",
+        route: "/assets"
+    }   
 }
 
 export default DevConfig;
--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -8,6 +8,22 @@ const application: express.Application = express();
 
 application.use(cors());
 application.use(express.json());
+// application.use('/assets', express.static("./static", {
+//     index: false,
+//     dotfiles: "deny",
+//     cacheControl: true,
+//     etag: true,
+//     maxAge: 1000* 60* 60* 24
+// }));
+
+application.use(config.static.route, express.static(config.static.path, {
+    index: config.static.index,
+    dotfiles: config.static.dotfiles,
+    cacheControl: config.static.cacheControl,
+    etag: config.static.etag,
+    maxAge: config.static.maxAge
+}));
+
 
 application.get('/about', (req,res) => {
     res.send([{
