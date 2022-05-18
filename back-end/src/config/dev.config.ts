bd3e33acfe18b02b2f9b46668350cb35096aef9b
ibjelic <bjelic.ilija@yahoo.com>
2022-05-18 20:32:04 +0200

    Hello world node and ts

---
+++ back-end/src/config/dev.config.ts
@@ -0,0 +1,9 @@
+import IConfig from "./IConfig.interface"
+
+const DevConfig: IConfig = {
+    server : {
+        port: 10000
+    }
+}
+
+export default DevConfig;
--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,1 +1,28 @@
-console.log("Radi!");
+import * as express from "express";
+import * as cors from "cors";
+import IConfig from "./config/IConfig.interface";
+import DevConfig from "./config/dev.config";
+
+const config: IConfig = DevConfig;
+const application: express.Application = express();
+
+application.use(cors());
+application.use(express.json());
+
+application.get('/about', (req,res) => {
+    res.send([{
+        message: 'About'
+    },
+    {
+        message: 'About'
+    }]
+    )
+}
+);
+
+application.use( (req, res) => {
+    res.sendStatus(404);
+}
+);
+
+application.listen(config.server.port)
