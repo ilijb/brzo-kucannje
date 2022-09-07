309a385b1dae29c35b1c94de27a7aa29d727890f
ibjelic <bjelic.ilija@yahoo.com>
2022-09-07 09:37:57 +0200

    Dizajn glavne stranice

--- front-end/src/components/Application.tsx
+++ front-end/src/components/Application.tsx
@@ -6,6 +6,7 @@ import Login from './User/Login/Login';
 import UserRegisterPage from './User/Registration/UserRegisterPage';
 import Kategorija from './Kategorija/Kategorija';
 import Tekstovi from './Tekstovi/Tekstovi';
+import KucanjeApp from './KucanjeApp/KucanjeApp';
 
 function Application() {
   return (
@@ -14,6 +15,7 @@ function Application() {
       <Routes>
         <Route path='/register' element={<UserRegisterPage />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/tekst/:id'  element={<KucanjeApp />} />
         <Route path='/category/:id'  element={<Tekstovi />} />
         <Route path='/app'  element={<Kategorija />} />
         <Route path='/' element = {<div>Dobro dosli</div>} />
--- front-end/src/components/KucanjeApp/KucanjeApp.tsx
+++ front-end/src/components/KucanjeApp/KucanjeApp.tsx
@@ -1,7 +1,36 @@
 export default function KucanjeApp() {
     return (
-        <div>
-            <h1>KucanjeApp</h1>
+        <>
+
+        <div className="m-5"></div>
+            <div className="d-flex justify-content-end">
+                <select name="nivoi" className="form-control w-25">
+                    <option value={"veoma-sporo"}>Veoma sporo</option>
+                    <option value={"sporo"}>Sporo</option>
+                    <option value={"srednje"}>Srednje</option>
+                    <option value={"brzo"}>Brzo</option>
+                    <option value={"veoma-brzo"}>Veoma brzo</option>
+                </select>
+            </div>
+
+          <div className="m-5"></div>
+
+          <div className="w-75 border border-dark p-3 m-auto display-4" style={{borderWidth: "2px", borderStyle: "solid", background: "#6495ED"}}>
+            Test
+          </div>
+
+          <div className="m-5"></div>
+        <div className="w-75 m-auto">
+
+          <input type="text" id="userInput" name="userInput" className="form-control" style={{fontSize: 25}} />
+        
         </div>
+
+        <div className="m-5"></div>
+
+        <div className="w-50 m-auto">
+            <button id="zapocni" className="btn btn-success w-100 p-4" style={{fontSize: 25}}>Zapocni</button>
+        </div>
+        </>
     );
 }
